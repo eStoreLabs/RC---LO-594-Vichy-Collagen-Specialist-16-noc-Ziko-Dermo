7027d9b25acf99f1c6c09410711180006fb305c9
Jakub Marzewski <109849381+Jakub-WebDev@users.noreply.github.com>
2025-08-26 12:22:26 +0200

    Add files via upload

---
+++ rich-content/Vichy/krem-16/assets/scripts/script.js
@@ -0,0 +1,26 @@
+(() => {
+  const accTab = document.querySelectorAll("#es-rc #es-rc-content .es-acc-tab");
+
+  for (let i = 0; i < accTab.length; i++) {
+    accTab[i].addEventListener("click", function () {
+      this.classList.add("es-acc-active");
+      let accTabContent = this.nextElementSibling;
+
+      const hideContent = () => {
+        accTabContent.style.maxHeight = null;
+        this.classList.remove("es-acc-active");
+      };
+
+      const showContent = () => {
+        accTabContent.style.maxHeight = accTabContent.scrollHeight + "px";
+        this.classList.add("es-acc-active");
+      };
+
+      if (accTabContent.style.maxHeight) {
+        hideContent();
+      } else {
+        showContent();
+      }
+    });
+  }
+})();
